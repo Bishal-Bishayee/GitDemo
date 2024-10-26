2782dbd57378886c48d1c207709083706dbfed18
Bishal <bishalbisoi6371@gmail.com>
2024-10-26 18:43:35 +0530

    Added new class

--- A.java
+++ A.java
@@ -1,10 +1,13 @@
 class A{
 public static void main(String[] args){
 System.out.println("Welcome to GitHub");
-System.out.println("Welcome to GitHub");
-
-System.out.println("Welcome to GitHub");
-
+B b=new B();
+b.m1();
 }
+  void m1(){
+System.out.println("Hello M1");
+}
+}
+class B extends A{
 
 }
