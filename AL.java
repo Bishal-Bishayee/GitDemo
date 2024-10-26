bc0419382ccad80b5fa726f7468664020b36b07c
Bishal <bishalbisoi6371@gmail.com>
2024-10-26 18:32:53 +0530

    modified for AL

--- AL.java
+++ AL.java
@@ -1,7 +1,9 @@
 class A{
 public static void main(String[] args){
 System.out.println("Welcome to GitHub");
-System.out.println("Exploring to GitHub");
+m1();
 }
+static void m1(){
+System.out.println("Hi");
 
 }
